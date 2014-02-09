6453cff68c3e39d86a970dcddc870a41c4842af9
WeeDzCokie <weedz@live.se>
2014-02-09 18:09:15 +0100

    hello world!

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,8 @@ package main;
 
 public class Main {
 
-	public Main() {
-		System.out.println("Hello World1!");
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 	}
 
 }
