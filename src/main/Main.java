f4e71235f216de15ab5f3e683ed5952ecf116fbe
gogasos <gus.nabro@gmail.com>
2014-02-10 17:29:27 +0100

    Ny Gui

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,24 @@
 package main;
 
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-
+import java.awt.*; 
+import javax.swing.*;
+public class Main extends JFrame
+{
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+JPanel pane = new JPanel();
+  JButton pressme = new JButton("Press Me");
+  Main()        // the frame constructor
+  {
+    super("JPrompt Demo"); setBounds(100,100,300,200);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Container con = this.getContentPane(); // inherit main frame
+    con.add(pane);    // JPanel containers default to FlowLayout
+    pressme.setMnemonic('P'); // associate hotkey to button
+    pane.add(pressme); pressme.requestFocus();
+    setVisible(true); // make frame visible
+  }
+  public static void main(String args[]) {new Main();}
 }
