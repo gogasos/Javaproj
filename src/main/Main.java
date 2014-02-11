d85803106143a6a3c26b8efd954497585ecb71dc
WeeDzCokie <weedz@live.se>
2014-02-11 21:10:06 +0100

    derp.2..

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,12 +12,14 @@ class Frame extends JFrame {
 	public Frame() {
 		super("Flappy Bird sucks big tits");
 		setBounds(100,100,300,200);
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		Container con = getContentPane(); // inherit main frame
 		con.add(pane);    // JPanel containers default to FlowLayout
+		
 		pressme.setMnemonic('P'); // associate hotkey to button
-		pane.add(pressme); pressme.requestFocus();
+		pane.add(pressme);
+		pressme.requestFocus();
 		setVisible(true); // make frame visible
 	}
 	
