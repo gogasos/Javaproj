56735fd4204840b040eb97ccde00ef4dad4f4ecd
WeeDzCokie <weedz@live.se>
2014-02-11 21:08:16 +0100

    fixed shti..

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,23 +2,33 @@ package main;
 
 import java.awt.*; 
 import javax.swing.*;
-public class Main extends JFrame
-{
-  /**
-	 * 
-	 */
+
+class Frame extends JFrame {
+
 	private static final long serialVersionUID = 1L;
-JPanel pane = new JPanel();
-  JButton pressme = new JButton("tryckpåmigförfan");
-  Main()        // the frame constructor
-  {
-    super("Flappy Bird sucks big tits"); setBounds(100,100,300,200);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Container con = this.getContentPane(); // inherit main frame
-    con.add(pane);    // JPanel containers default to FlowLayout
-    pressme.setMnemonic('P'); // associate hotkey to button
-    pane.add(pressme); pressme.requestFocus();
-    setVisible(true); // make frame visible
-  }
-  public static void main(String args[]) {new Main();}
+	static JPanel pane = new JPanel();
+	static JButton pressme = new JButton("tryckpåmigförfan");
+	
+	public Frame() {
+		super("Flappy Bird sucks big tits");
+		setBounds(100,100,300,200);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container con = getContentPane(); // inherit main frame
+		con.add(pane);    // JPanel containers default to FlowLayout
+		pressme.setMnemonic('P'); // associate hotkey to button
+		pane.add(pressme); pressme.requestFocus();
+		setVisible(true); // make frame visible
+	}
+	
+}
+
+public class Main{
+	
+	
+	
+	public static void main(String args[]) {
+		new Frame();
+	}
+	
 }
